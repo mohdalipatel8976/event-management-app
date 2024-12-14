88fad59939be7c5c44986a608139975f045f840e
Patel Mohammedali <mohdalipatel8976@gmail.com>
2024-12-14 21:39:26 +0530

    installed tailwind and added in the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="h-screen flex justify-center items-center bg-gray-700">
+      <h1 className="text-white text-3x1 font-bold">Homepage</h1>
     </div>
-  )
+  );
 }
 
 export default App

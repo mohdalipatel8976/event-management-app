35a5eb89f01b8888559f22a09c45e18511251d0e
Patel Mohammedali <mohdalipatel8976@gmail.com>
2024-12-22 14:26:52 +0530

    Login and Register API integrated

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const app = express();
 require('dotenv').config();
 
 connectMongoDB();
+
+app.use(express.json());
+app.use("/api/users", require("./routes/users-route")); 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
